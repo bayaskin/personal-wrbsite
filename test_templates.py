3e141ce1499c9a694950fe5a70b6e808622e489a
bayaskin <bayas.edu.ne@gmail.com>
2020-06-23 00:04:53 -0500

    added tests, init

---
+++ test_templates.py
@@ -0,0 +1,23 @@
+import pytest
+from .myapp import app
+
+@pytest.fixture
+def test_client():
+    flask_app = app.test_client()
+    return flask_app
+
+def setUp(client):
+    response = client.get('/')
+
+def test_home_page(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b'Bayaskhalan Tsyzhipov' in response.data
+
+#def test_homepage_template(self):  
+#    self.assert_template_used(self.response, 'home.html')
